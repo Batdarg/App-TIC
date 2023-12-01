3a08ff23efb657ce1d4a3add1890cd8eaff7cd7a
BitingSet <ahernandezlopez44@gmail.com>
2023-11-30 23:08:11 -0600

    Descripción de los cambios

---
+++ js/registroParticipantes.js
@@ -0,0 +1,45 @@
+const url_api = 'http://localhost:5238/api/insertarParticipantes';
+
+function clearForm() {
+  $('#nombreInput').val('');
+  $('#apellidosInput').val('');
+  $('#emailInput').val('');
+  $('#inputTwitter').val('');
+}
+
+function obtenerValorRadioSeleccionado() {
+    var elementosRadio = document.getElementsByName("opcion");
+    for (var i = 0; i < elementosRadio.length; i++) {
+        if (elementosRadio[i].checked) {
+            return elementosRadio[i].value;
+        }
+    }
+    return null;
+}
+
+function submitForm() {
+
+    var avatarSeleccionado = obtenerValorRadioSeleccionado();
+  var formData = {
+      nombre: $('#nombreInput').val(),
+      apellidos: $('#apellidosInput').val(),
+      email: $('#emailInput').val(),
+      twitter: $('#inputTwitter').val(),
+      avatar: avatarSeleccionado
+  };
+
+  $.ajax({
+      type: 'POST',
+      url: url_api,
+      data: JSON.stringify(formData),
+      contentType: 'application/json',
+      success: function(response) {
+        alert('Registro enviado exitosamente');
+        console.log('Respuesta del servidor:', response);
+        clearForm();
+      },
+      error: function(error) {
+          console.error('Error en la solicitud:', error);
+      }
+  });
+}
